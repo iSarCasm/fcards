28759c23fa85eee256e189b4bee041a00bf1d0cb
Igor Tsykalo <sarcasm008@gmail.com>
2022-04-10 16:24:49 +0200

    load defaults

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ Bundler.require(*Rails.groups)
 module Fcards
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 6.1
 
     config.eager_load_paths << Rails.root.join('lib')
 
